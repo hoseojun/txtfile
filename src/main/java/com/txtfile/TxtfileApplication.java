f37f63b235ddf360bb2c5f526204f286590fd7b5
hoseojun <stjwhg44@naver.com>
2022-12-19 16:44:01 +0900

    .

---
+++ src/main/java/com/txtfile/TxtfileApplication.java
@@ -0,0 +1,13 @@
+package com.txtfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxtfileApplication.class, args);
+	}
+
+}
